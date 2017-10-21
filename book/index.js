c8b1058661ec5f873b49b2e9eb0dad9e7e267a54
Ryan Allred <Ryan@Allred.xyz>
2017-10-20 20:05:44 -0600

    Add a little more transform to the book pages.

--- book/index.js
+++ book/index.js
@@ -10,26 +10,28 @@ const dragState = {x: 0, vel: null, lastX: null}
   const location = -dragState.x/pageWidth
   const closestPage = Math.min(Math.max(0, Math.round(location)), pages.length-1)
   if (dragState.vel === null || dragState.vel !== 0 || location !== closestPage) {
-    dragState.x += dragState.vel
-    dragState.x = Math.min(0, dragState.x)
-    dragState.x = Math.max(-pageWidth * (pages.length-1), dragState.x)
-    dragState.vel *= .9
     const location = -dragState.x/pageWidth
     if (dragState.lastX === null) {
-      const pull = (location - closestPage) * 80
-      dragState.vel = (dragState.vel * 4 + pull)/5
+      const pull = (location - closestPage)*50
+      dragState.vel += pull
+      dragState.vel *= .5
+    } else {
+      dragState.vel *= .9
     }
     const panelIndex = Math.floor(location)
     const panelOffset = (location - panelIndex) * pageWidth
     pages.forEach((page, index)=>{
       if (index < panelIndex)
-          page.style.transform = `translateX(-110%)`
+          page.style.transform = `translateX(-200%)`
       else if (index > panelIndex)
-        page.style.transform = `translateX(0)`
+        page.style.transform = `translateX(${(pageWidth-panelOffset)/3}px) translateZ(${-(pageWidth-panelOffset)/3}px) rotateY(${-((pageWidth-panelOffset)/pageWidth)*30}deg)`
       else
         page.style.transform = `translateX(${-panelOffset}px)`
     })
 
+    dragState.x += dragState.vel
+    dragState.x = Math.min(0, dragState.x)
+    dragState.x = Math.max(-pageWidth * (pages.length-1), dragState.x)
     if (Math.abs(dragState.vel) < .01) dragState.vel = 0
   }
   window.requestAnimationFrame(anim)
